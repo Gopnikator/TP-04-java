eeecbfcea05f986f175316c0abef4ef3f71e8548
Unknown <alexandre1arnaud@gmail.com>
2018-03-06 11:08:13 +0100

    Classe TestEtudiant

---
+++ TP04/TestEtudiant.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class TestEtudiant here.
+ *
+ * @author Alexandre Arnaud - Corentin Besnard
+ * @version 6 mars 2018
+ */
+
+
+import java.util.Scanner;
+
+
+public class TestEtudiant
+{
+    public static void main(String[] args){
+        String nomEtu;
+        String prenomEtu;
+                
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Veuillez saisir le nom de l'étudiant :");
+        nomEtu = sc.nextLine();
+        
+        System.out.println("Veuillez saisir le prénom de l'étudiant :");
+        prenomEtu = sc.nextLine();
+
+        
+        Etudiant x = new Etudiant (prenomEtu, nomEtu);      
+        
+        
+        
+    }
+   
+}
