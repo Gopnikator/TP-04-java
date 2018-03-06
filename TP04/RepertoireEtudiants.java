cd97d69d1637af7e9544171ee38a6218f5ec2ce4
Unknown <alexandre1arnaud@gmail.com>
2018-03-06 11:51:53 +0100

    Debut classe RepertoireEtudiant

---
+++ TP04/RepertoireEtudiants.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class RepertoireEtudiants here.
+ *
+ * @author Alexandre Arnaud - Corentin Besnard
+ * @version 6 mars 2018
+ */
+public class RepertoireEtudiants
+{
+    // instance variables - replace the example below with your own
+    private String nomEtu;
+    private String prenomEtu;
+    private int numeroEtu;
+
+    /**
+     * Constructor for objects of class RepertoireEtudiants
+     */
+    public RepertoireEtudiants()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public boolean ajouteEtudiant(Etudiant e)
+    {
+        
+    }
+    
+    public int getNbEtudiant()
+    {
+        
+    }
+ 
+    public String getNomEtudiant(int numeroEtu)
+    {
+        
+    }
+    
+    public String getNumeroEtudiant(String nomEtu, String prenomEtu)
+    {
+        
+    }
+    
+    public boolean supprimeEtudiant(Etudiant e){
+        
+    }
+}
