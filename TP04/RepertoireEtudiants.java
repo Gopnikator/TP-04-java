97aa54f3665abc27d3fc4f36d17e0a3bfb35294c
Unknown <alexandre1arnaud@gmail.com>
2018-03-07 18:04:14 +0100

    repertoire

--- TP04/RepertoireEtudiants.java
+++ TP04/RepertoireEtudiants.java
@@ -8,16 +8,24 @@
 public class RepertoireEtudiants
 {
     // instance variables - replace the example below with your own
-    private String nomEtu;
-    private String prenomEtu;
-    private int numeroEtu;
+    private int nbMax;
+    private int nbE;
+    private String tabEtu[];
+    private int i = 0;
 
     /**
      * Constructor for objects of class RepertoireEtudiants
      */
     public RepertoireEtudiants()
     {
-        
+        int nbMax = 50;
+        int nbE = 0;
+    }
+    
+    public RepertoireEtudiants(int nbMaxi)//, int nbEtu)
+    {
+        nbMax = nbMaxi;
+        //nbE = nbEtu;     
     }
 
     /**
@@ -25,30 +33,39 @@ public class RepertoireEtudiants
      */
     public boolean ajouteEtudiant(Etudiant e)
     {
-        
+         if (nbE < nbMax){
+             
+             i++;
+             nbE++;
+             return true;
+         } else {
+             
+             return false;
+         }
+         
     }
     
     public int getNbEtudiant()
     {
-        
+        return nbE;
     }
  
     public String getNomEtudiant(int numeroEtu)
     {
-        
+        // boucle for avec if
     }
     
     public String getPrenomtudiant(int numeroEtu)
     {
-        
+        // boucle for avec if
     }
     
-    public String getNumeroEtudiant(String nomEtu, String prenomEtu)
+    public String getNumeroEtudiant(String nomEtu)
     {
-        
+        // boucle for avec if
     }
     
     public boolean supprimeEtudiant(Etudiant e){
-        
+        // Si etudiant est dans la liste alors on le supprime, on fait baisser l'indice si necessaire et on retourne vrai sinon on retourne message erreur + faux
     }
 }
